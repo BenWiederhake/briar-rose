872d531d0b86a4b5cfe9babb9bfd9ee114bfa3d6
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2017-12-15 23:17:22 +0100

    All the config and option parsing stuff

--- briar_rose.py
+++ briar_rose.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from os import environ
-from sys import stdout, stderr
+import sys
 import subprocess
 #from pid import Pidfile
 
@@ -69,9 +69,92 @@ def parse_rules(rules, err_fd):
         else:
             print('PID set unchanged', file=err_fd)
     print('Parsing completed.', file=err_fd)
-    # return pids
+    return pids
+
+
+LAST_PIDS = {}
+
+
+def load_pids(config_path, err_fd):
+    try:
+        fd = open(config_path, 'r')
+        # If you really need to stop a program of unknown pid
+        # with \r or \n in its name, then you're fucked anyway.
+        new_pids = parse_rules([line.rstrip('\r\n') for line in fd.readlines()], err_fd)
+        fd.close()  # If this leaks, the program is about to die anyway.
+        LAST_PIDS = new_pids
+    except OSError as e:
+        print('Cannot read config file: {}'.format(e), file=err_fd)
+
+    return LAST_PIDS
+
+
+def run_debug(config_path):
+    pids = load_pids(config_path, sys.stdout)
+    print('So I would stop {} pids now.'.format(len(pids)))
+
+
+# def run_daemon(config_path, pidfile_path):
+
+
+def default_pidfile_path():
+    if 'XDG_RUNTIME_DIR' in environ:
+        return '{}/briar_rose.pid'.format(environ['XDG_RUNTIME_DIR'])
+    if 'USER' in environ:
+        return '/tmp/{}-briar_rose.pid'.format(environ['USER'])
+    return '/tmp/briar_rose.pid'
+
+
+def run_args(args):
+    config_path = None  # Must be replaced by dynamic default value
+    #'briar-rose.config'
+    pidfile_path = None  # Must be replaced by dynamic default value
+    is_debug = False
+
+    while len(args) > 0:
+        if args[0] == '--debug':
+            if is_debug:
+                print('Duplicate option --debug!', file=sys.stderr)
+                exit(1)
+            is_debug = True
+            args = args[1:]
+        elif args[0] == '--pidfile':
+            if pidfile_path is not None:
+                print('Duplicate option --pidfile!', file=sys.stderr)
+                exit(1)
+            if len(args) < 2:
+                print('Missing argument to --pidfile!', file=sys.stderr)
+                exit(1)
+            pidfile_path = args[1]
+            args = args[2:]
+        elif args[0] == '--config':
+            if config_path is not None:
+                print('Duplicate option --config!', file=sys.stderr)
+                exit(1)
+            if len(args) < 2:
+                print('Missing argument to --config!', file=sys.stderr)
+                exit(1)
+            config_path = args[1]
+            args = args[2:]
+        # TODO: How about '--help'?
+        else:
+            print('Unknown option {}!'.format(args[0]), file=sys.stderr)
+            exit(1)
+
+    if config_path is None:
+        config_path = 'briar_rose.config'
+
+    if pidfile_path is None:
+        pidfile_path = default_pidfile_path()
+
+    if is_debug:
+        print('Would use pidfile at {}'.format(pidfile_path))
+        print('Loading configuration from {}'.format(config_path))
+        run_debug(config_path)
+    else:
+        raise NotImplementedError()
+        # run_daemon(config_path, pidfile_path)
 
 
 if __name__ == '__main__':
-    print('Nothing implemented yet!')
-    exit(1)
+    run_args(sys.argv[1:])
